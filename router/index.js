2518228361279f624d4dcf95ed5343c20a0effd8
Fabio Colletto <149891035+fabiocolletto@users.noreply.github.com>
2025-10-22 04:43:01 -0300

    Corrige abertura do painel inicial pelo cabeçalho

--- router/index.js
+++ router/index.js
@@ -23,6 +23,8 @@ class Router {
     }
 
     if (this.#currentRoute === route) {
+      renderView(targetView);
+      logInfo('router.navigate', `Rota "${route}" já estava ativa. View recarregada.`);
       return;
     }
 
