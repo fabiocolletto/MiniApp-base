a7805405d070d237acf1a3f81f026c30eb31e53f
Fabio Colletto <149891035+fabiocolletto@users.noreply.github.com>
2025-10-25 09:00:48 -0300

    Create temporary version preview panel

---
+++ temp/version-panel/config.js
@@ -0,0 +1,5 @@
+export const GITHUB_OWNER = 'seu-usuario';
+export const GITHUB_REPO = 'MiniApp-base';
+export const DEFAULT_BRANCH = 'main';
+export const PREVIEW_PATH = 'index.html';
+export const MAX_BRANCHES = 50;
