39b8da23a96d6059f8ac367e26988ccd5a0b206d
Fabio Colletto <149891035+fabiocolletto@users.noreply.github.com>
2025-11-07 11:02:27 -0300

    Delete scripts directory

--- scripts/data/system-release-source.js
+++
@@ -1,7 +0,0 @@
-const SYSTEM_RELEASE_SOURCE = Object.freeze({
-  version: 'v4.1.0',
-  publishedAt: '2025-11-03T04:43:01-03:00',
-  changelogPath: './Log.md',
-});
-
-export default SYSTEM_RELEASE_SOURCE;
