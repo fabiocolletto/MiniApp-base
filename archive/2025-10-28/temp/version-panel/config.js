ca99174ea1835b0797b1e3a8857c800a82dbf8ca
Fabio Colletto <149891035+fabiocolletto@users.noreply.github.com>
2025-11-07 11:08:51 -0300

    Delete archive/2025-10-28 directory

--- archive/2025-10-28/temp/version-panel/config.js
+++
@@ -1,5 +0,0 @@
-export const GITHUB_OWNER = 'seu-usuario';
-export const GITHUB_REPO = 'MiniApp-base';
-export const DEFAULT_BRANCH = 'main';
-export const PREVIEW_PATH = 'index.html';
-export const MAX_BRANCHES = 50;
